b7faf9499bb9096607be4c9bce65ca14a20d75e9
Dennis Kjellin <denniskjellin@hotmail.com>
2023-03-22 14:17:59 +0100

    simple fetch, trying out the connection to Supabase

--- frontend/app.vue
+++ frontend/app.vue
@@ -3,3 +3,12 @@
     <NuxtWelcome />
   </div>
 </template>
+
+
+<script setup lang="ts">
+const client = useSupabaseClient();
+
+const { data: users } = await useAsyncData('users', async () => client.from('users').select('*').order('created_at', { ascending: true }))
+
+console.log(users.value)
+</script>
