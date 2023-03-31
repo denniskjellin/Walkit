ba5f8946999c7ab1d105accc7bc72140df2d3ef0
Dennis Kjellin <denniskjellin@hotmail.com>
2023-03-31 10:02:57 +0200

    removing files I dont need, testing so supabase is working with fetching data

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,6 +1,7 @@
 /* import { defineNuxtConfig } from 'nuxt' */
 
 export default defineNuxtConfig({
+
   css: ['@/assets/css/tailwind.css', '@fortawesome/fontawesome-free/css/all.css'],
   modules: ['@nuxtjs/supabase', '@nuxtjs/tailwindcss'],
 
