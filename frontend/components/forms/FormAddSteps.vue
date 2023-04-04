81e71a8f16eaa0dfd397818bf007c3e0285f87fc
Dennis Kjellin <denniskjellin@hotmail.com>
2023-04-04 18:31:34 +0200

    updated

--- frontend/components/forms/FormAddSteps.vue
+++ frontend/components/forms/FormAddSteps.vue
@@ -3,11 +3,23 @@
       <h2>Registrera steg</h2>
       <div class="input-label-container">
         <label class="label-add-steps-form" for="date">Datum</label>
-        <input class="input-add-steps-form" id="date" type="date" v-model="date" />
+        <input
+          class="input-add-steps-form"
+          id="date"
+          ref="test"
+          type="date"
+          v-model="date"
+        /> 
       </div>
       <div class="input-label-container">
         <label class="label-add-steps-form" for="steps">Antal steg</label>
-        <input class="input-add-steps-form" id="steps" type="number" v-model="steps" />
+        <input
+          class="input-add-steps-form"
+          id="steps"
+          type="number"
+          v-model="steps"
+          
+        /> 
       </div>
       <div class="add-steps-form__submit input-label-container">
         <button @click.prevent="insertSteps" class="btn-bg-clay-black">
@@ -17,11 +29,27 @@
     </form>
   </template>
   
-  <script>
+  <script setup>
+      const supabase = useSupabaseClient();
 
+      const date = ref(null);
+      const steps = ref(0);
+  
+      const insertSteps = async () => {
+        try {
+          console.log(date.value);
+          console.log(steps.value);
 
-  export default {
-
-  }
+          const { data, error } = await supabase
+            .from("steps")
+            .insert([{ date: date.value, steps: steps.value, user_id: "98da1930-5d24-4e2d-ac5f-d6f0bfa6ce63", destination_id: "86ee5f9b-31eb-46ca-b6ec-ff848f845e46" }]);
+  
+          if (error) throw error;
+          alert("Steg tillagda!");
+        } catch (error) {
+          console.log("Error inserting steps:", error.message);
+          alert("Error inserting steps!");
+        }
+      };
   </script>
   
