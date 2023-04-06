760de07a5af567285a9b9c4264e70e59e0d352e3
Dennis Kjellin <denniskjellin@hotmail.com>
2023-04-06 09:52:38 +0200

    some basic styling on the add destination form

--- frontend/pages/admin.vue
+++ frontend/pages/admin.vue
@@ -1,82 +1,86 @@
 <!-- pages/index.vue -->
 <template>
-    <div class="container">
-      <div class="left-column">
-        <article>
-          <h1>hey</h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
-            libero nobis quos rerum totam exercitationem voluptatibus id numquam?
-            Quasi ex libero fuga expedita facere cumque nostrum nesciunt,
-            accusamus ipsa pariatur!
-          </p>
-        </article>
-      </div>
-      <div class="right-column">
-        <article>
-          <h1>hey</h1>
-          <form>
-            <label for="name">Name:</label>
-            <input type="text" id="name" name="name" required>
-  
-            <label for="email">Email:</label>
-            <input type="email" id="email" name="email" required>
-  
-            <label for="message">Message:</label>
-            <textarea id="message" name="message" rows="5" required></textarea>
-  
-            <button type="submit">Send</button>
-          </form>
-        </article>
-      </div>
+  <div class="container">
+    <div class="left-column">
+      <article>
+        <h1>Get data</h1>
+        <ul>
+      <li v-for="destination in destinations" :key="destination.id">
+        Destination ID: {{ destination.from }}{{ destination.id }}
+      </li>
+    </ul>
+      </article>
     </div>
-  </template>
-  
-  <script setup>
-  const { auth } = useSupabaseAuthClient();
-  const user = useSupabaseUser();
-  
-  definePageMeta({
-    middleware: "auth",
-    layout: "default",
-  });
-  </script>
-  
-  <style lang="scss" scoped>
-  /* add any custom styles for the form here */
-  form {
-    display: flex;
-    flex-direction: column;
+    <div class="right-column">
+      <article>
+        <h1>hey</h1>
+        <form class="form-admin">
+          <div class="input-section">
+          <label class="label-form" for="name">Name:</label>
+          <input class="input-form" type="text" id="name" name="name" required />
+        </div>
+        <div class="input-section">
+          <label class="label-form" for="email">Email:</label>
+          <input class="input-form" type="email" id="email" name="email" required />
+        </div>
+          <label for="message">Message:</label>
+          <textarea class="input-section" id="message" name="message" rows="5" required></textarea>
+
+          <button class="button-submit" type="submit">Send</button>
+        </form>
+      </article>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const supabase = useSupabaseClient();
+let destinations = [];
+
+// fetch destinations from Supabase
+const fetchDestinations = async () => {
+  try {
+    let { data: response, error } = await supabase
+      .from("destinations")
+      .select("*")
+      .order("id", { ascending: true });
+
+    if (response) {
+      console.log(response);
+      destinations = response;
+    }
+  } catch (error) {
+    console.log(error);
   }
-  
-  label {
-    font-weight: bold;
-    margin-bottom: 0.5rem;
-  }
-  
-  input,
-  textarea {
-    padding: 0.5rem;
-    margin-bottom: 1rem;
-    border-radius: 0.25rem;
-    border: 1px solid #ccc;
-  }
-  
-  button {
-    padding: 0.5rem 1rem;
-    border-radius: 0.25rem;
-    border: none;
-    background-color: #007bff;
-    color: #fff;
-    cursor: pointer;
-  }
-  
-  button:hover {
-    background-color: #0069d9;
-  }
-  
-  button:active {
-    background-color: #005cbf;
-  }
-  </style>
-  
+};
+
+definePageMeta({
+  middleware: "auth",
+  layout: "default",
+});
+</script>
+
+<style lang="scss" scoped>
+/* add any custom styles for the form here */
+form {
+  display: flex;
+  flex-direction: column;
+}
+
+label {
+  font-weight: bold;
+  margin-bottom: 0.5rem;
+}
+
+input,
+textarea {
+  padding: 0.5rem;
+  margin-bottom: 1rem;
+  border-radius: 0.25rem;
+  border: 1px solid #ccc;
+}
+
+
+
+
+</style>
