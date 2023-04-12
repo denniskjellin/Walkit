3f68b6ae08d8761e7d2a0d6bb6ec3d31f462ade0
Dennis Kjellin <denniskjellin@hotmail.com>
2023-04-12 19:47:38 +0200

    Removed aria labels from here because it gave me an error/warning in the console

--- frontend/components/header/HeaderNav.vue
+++ frontend/components/header/HeaderNav.vue
@@ -2,19 +2,19 @@
   <header>
     <nav class="main-header">
       <div class="profile-container">
-        <NuxtLink to="/profile" title="Profile page" aria-label="Min sida"
+        <NuxtLink to="/profile" title="Profile page"
           ><i class="fas fa-user"></i>
         </NuxtLink>
         <p class="menu-text">Profil</p>
       </div>
-      <NuxtLink to="/" class="walkit-logo" aria-label="Start">
+      <NuxtLink to="/" class="walkit-logo">
         <LogosLogoMain />
       </NuxtLink>
       <div class="menu-foldouts">
         <!-- Add steps form component, icon in the navbar is inside AddStepsComponent-->
-        <HeaderAddSteps aria-label="Lägg till steg"/>
+        <HeaderAddSteps />
         <!-- Hamburger Menu, links is inside the component for navigation through the fold-out menu -->
-        <HeaderHamurgerMenu aria-label="huvudmeny" />
+        <HeaderHamurgerMenu />
       </div>
     </nav>
   </header>
