6e87475aa469197b9a77202b179cef75e6970339
Dennis Kjellin <denniskjellin@hotmail.com>
2023-04-16 10:15:56 +0200

    function for calculating remaining steps

---
+++ frontend/components/stepsleftblock.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="content-div">
+    <section class="section-block steps-left">
+      <p class="h1-s">{{ remainingSteps }}</p>
+      <p>steg kvar!</p>
+      <p
+        v-if="errorMsg"
+        class="error-box center"
+        role="alert"
+        aria-live="assertive"
+      >
+        {{ errorMsg }}
+      </p>
+    </section>
+  </div>
+</template>
+
+<script setup>
+const supabase = useSupabaseClient();
+const remainingSteps = ref(null);
+
+const errorMsg = ref("");
+
+const getRemainingSteps = async () => {
+  try {
+    // Get active destination
+    const { data: activeDestinations, error } = await supabase
+      .from("destinations")
+      .select("*")
+      .eq("is_active", true);
+
+    if (error) throw error;
+    // Get the ID of the first active destination (there should only be one)
+    const destinationId = activeDestinations[0]?.id;
+    if (!destinationId) {
+      if (error) throw error;
+    }
+
+    // Get all steps for the active destination
+    const { data: stepsData, error: stepsError } = await supabase
+      .from("steps")
+      .select("steps")
+      .eq("destination_id", destinationId);
+
+    if (stepsError) throw stepsError;
+
+    // Sumary of all steps added to the active destination
+    const totalSteps = stepsData.reduce((total, current) => total + current.steps, 0);
+
+    // Get the goal number of steps for the active destination
+    const { data: destinationData, error: destinationError } = await supabase
+      .from("destinations")
+      .select("steps_goal")
+      .eq("id", destinationId);
+
+    // If there is an error, throw it
+    if (destinationError) throw destinationError;
+    const stepsGoal = destinationData[0]?.steps_goal;
+
+    // Count the remaining steps and set the value of the ref "remainingSteps"
+    const remainingStepsValue = stepsGoal - totalSteps;
+    remainingSteps.value = remainingStepsValue;
+  } catch (error) {
+    errorMsg.value = "Kan ej läsa in data.. :(";
+  }
+};
+// Run getRemainingSteps when the component is mounted
+onMounted(getRemainingSteps);
+</script>
