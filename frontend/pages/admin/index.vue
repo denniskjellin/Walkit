bc57eb234bc9e642b129174becfe2372507a77e7
Dennis Kjellin <denniskjellin@hotmail.com>
2023-05-05 11:44:06 +0200

    css changes on card

--- frontend/pages/admin/index.vue
+++ frontend/pages/admin/index.vue
@@ -1,10 +1,24 @@
 <template>
-    <div>
-        <h1>Admin index</h1>
-    </div>
+  <div class="admin-page">
+    <section class="section-block">
+      <h1 class="h1-s">Kontrollpanel</h1>
+      <div class="card-wrapper">
+        <NuxtLink to="/admin/destinationer">
+          <div class="card">
+            <h2 class="h2-s">Destinationer</h2>
+            <span><SvgGoalblockIco class="icon-card" /></span>
+          </div>
+        </NuxtLink>
+
+        <NuxtLink to="/admin/destinationer">
+          <div class="card">
+            <h2 class="h2-s">Aktivitetslista</h2>
+            <span><SvgGoalprogIco class="icon-card" /></span>
+          </div>
+        </NuxtLink>
+      </div>
+    </section>
+  </div>
 </template>
 
-<script setup>
-
-</script>
-
+<script setup></script>
