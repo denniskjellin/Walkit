0527c1accc79e40d80b4bd15fd30b73054b4b71f
Dennis Kjellin <denniskjellin@hotmail.com>
2023-05-06 14:59:22 +0200

    edit steps in progress

---
+++ frontend/pages/profile/steps/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <ProfileHeroImg />
+  <ProfileUserDetails />
+  <StepsEditTable />
+</template>
+
+<script setup lang="ts">
+import StepsEditTable from '~~/components/profile/StepsEditTable.vue';
+
+const user = useSupabaseUser();
+// Redirect to the login page if the user is not signed in
+watchEffect(() => {
+  if (!user.value) {
+    return navigateTo("/login");
+  }
+});
+
+definePageMeta({
+  middleware: "auth",
+  layout: "default",
+});
+</script>
+
+<style scoped></style>
