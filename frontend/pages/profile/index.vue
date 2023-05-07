f3c451270914c5cd640cc714819aeefabf5e648f
Dennis Kjellin <denniskjellin@hotmail.com>
2023-05-07 12:12:01 +0200

    change component order

--- frontend/pages/profile/index.vue
+++ frontend/pages/profile/index.vue
@@ -3,33 +3,11 @@
   <ProfileHeroImg />
   <ProfileUserDetails />
   <ProfileSummaryCirclesProfile />
-  <ProfileWeekStats />
-  <!-- <ProfileStepsEditTable /> -->
+  <!-- <ProfileWeekStats /> -->
+  <ProfileStepsEditTable />
 </template>
 
 <script setup>
-// // const { auth } = useSupabaseAuthClient();
-// const user = useSupabaseUser();
-
-// // Logout the user
-// const userLogout = async () => {
-//   // Sign out the user
-//   await auth.signOut();
-
-//   // Redirect to the login page if the user is not signed in
-//   watchEffect(() => {
-//     if (!user.value) {
-//       return navigateTo("/login");
-//     }
-//   });
-//   // Redirect to the home page if the user is signed in
-// };
-
-// definePageMeta({
-//   middleware: "auth",
-//   layout: "default",
-// });
-
 const user = useSupabaseUser();
 // Redirect to the login page if the user is not signed in
 watchEffect(() => {
