4a0aff25643ab5c89bf1d77a36ca6bf87a3d29e3
Dennis Kjellin <denniskjellin@hotmail.com>
2023-05-11 08:27:53 +0200

    minor map styling

--- frontend/components/map/map.vue
+++ frontend/components/map/map.vue
@@ -4,13 +4,13 @@
 
 <script setup>
 let map;
-let center = [36.190522, 27.940551]; // default center location
-let start = [62.357112, 17.31843]; // Sundsvall
-let end = [36.190522, 27.940551]; // Rhodos
+let currentLocation = [36.190522, 27.940551]; // default center location
+let startLocation = [62.357112, 17.31843]; // Sundsvall
+let endLocation = [36.190522, 27.940551]; // Rhodos
 onMounted(() => {
   map = L.map("map", {
-    center: center,
-    zoom: 6,
+    center: currentLocation,
+    zoom: 8,
   });
   L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
     maxZoom: 19,
@@ -18,11 +18,16 @@ onMounted(() => {
       '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
   }).addTo(map);
 
-  let startMarker = L.marker(start).addTo(map);
-  let endMarker = L.marker(end).addTo(map);
+  let startMarker = L.marker(startLocation).addTo(map);
+  let endMarker = L.circle(endLocation, {
+    color: "ff00ff",
+    fillColor: "blue",
+    fillOpacity: 0.5,
+    radius: 30000,
+  }).addTo(map);
 
-  let destinationLine = L.polygon([start, end], {
-    color: "red",
+  let destinationLine = L.polygon([startLocation, endLocation], {
+    color: "#ff00ff",
     fillColor: "#f03",
     fillOpacity: 0.5,
   }).addTo(map);
