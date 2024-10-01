6534cefda0e8b5f003a24a15a21135173940002a
Dennis Kjellin <dennis.kjellin@knowit.se>
2024-10-01 08:44:15 +0200

    links footer

--- frontend/components/footer.vue
+++ frontend/components/footer.vue
@@ -4,19 +4,24 @@
       <div class="footer-logo">
         <LogosLogoFooter />
         <p>Created by dennis.kjellin@knowit.se</p>
+        <NuxtLink class="footer-external-link" to="https://www.knowit.se/misc/hantering-av-personuppgifter/">
+          Hantering av personuppgifter
+          <span class="sr-only">Extern länk</span>
+        </NuxtLink>
       </div>
       <div class="footer-social">
         <ul>
           <li>
-            <a aria-label="Följ oss på Facebook" target="_blank" href="https://www.facebook.com/KnowitExperienceSweden"><i
-                class="fab fa-facebook-f"></i></a>
+            <a aria-label="Följ oss på Facebook" href="https://www.facebook.com/KnowitExperienceSweden"><i class="fab fa-facebook-f"><span
+                  class="sr-only">Extern länk</span></i></a>
           </li>
           <li>
-            <a aria-label="Följ oss på Twitter" target="_blank" href="https://twitter.com/knowitab"><i class="fab fa-x"></i></a>
+            <a aria-label="Följ oss på X" href="https://twitter.com/knowitab"><i class="fab fa-x"><span class="sr-only">Extern
+                  länk</span></i></a>
           </li>
           <li>
-            <a aria-label="Följ oss på Instagram" target="_blank" href="https://www.instagram.com/knowitexperience_se/"><i
-                class="fab fa-instagram"></i></a>
+            <a aria-label="Följ oss på Instagram" href="https://www.instagram.com/knowitexperience_se/"><i class="fab fa-instagram"><span
+                  class="sr-only">Extern länk</span></i></a>
           </li>
         </ul>
       </div>
